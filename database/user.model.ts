daa7dc9c76fd5a3f9cf92a08c5e182707b7688b2
Navdeep <digitalnavtech6@gmail.com>
2026-01-21 15:13:14 +0200

    first commit

---
+++ database/user.model.ts
@@ -0,0 +1,32 @@
+import { model, models, Schema } from "mongoose";
+
+export interface IUser {
+  id: string | undefined;
+  name: string;
+  username: string;
+  email: string;
+  bio?: string;
+  image?: string;
+  location?: string;
+  portfolio?: string;
+  reputation?: number;
+}
+
+const UserSchema = new Schema<IUser>(
+  {
+    name: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    bio: { type: String },
+    image: { type: String },
+    location: { type: String },
+    portfolio: { type: String },
+    reputation: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
+
+const User = models?.User || model<IUser>("User", UserSchema);
+
+export default User;
+
